9b8497fc63f7b6a4cf9d5f5cefffb935f80270ac
PiratheeshG <piratheesh.gunaseelan@ontariotechu.net>
2024-11-23 23:55:39 -0500

    Again

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("view engine", "ejs");
 
 // Database Connection
 mongoose
-  .connect(process.env.mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("Database connection error:", err));
 
@@ -29,9 +29,9 @@ app.use("/milestones", require("./routes/milestone-routes"));
 
 // Home Route
 app.get("/", (req, res) => {
-  res.render("layouts/main", { content: "index" }); // Dynamically include index.ejs
+  res.render("layouts/main", { content: "index" });
 });
 
 // Start Server
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
